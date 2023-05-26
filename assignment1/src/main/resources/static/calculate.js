ae3d3536d6f3d3b9fa25d422d7abac2578fb9417
bevannutakor <bevannutakor@icloud.com>
2023-05-25 20:07:25 -0700

    created javascript file

---
+++ assignment1/src/main/resources/static/calculate.js
@@ -0,0 +1,27 @@
+var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03, 
+    49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
+
+var histogram = {
+    Aplus: 0,
+    A: 0,
+    Aminus: 0,
+    Bplus: 0,
+    B: 0,
+    Bminus: 0,
+    Cplus: 0,
+    C: 0,
+    Cminus: 0,
+    D: 0,
+    F: 0,
+
+}
+
+function groupGrades(){
+
+}
+//Events for changes to lower bounds
+
+
+function submitGrade(){
+
+}
